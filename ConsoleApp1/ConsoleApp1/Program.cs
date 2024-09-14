0b50859013ec664c2bb54cd5741bbd2b0fa894de
Vladimir Mansurov <70720123+VolodyaHoi@users.noreply.github.com>
2024-09-14 13:51:16 +0300

    Add function

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,46 +9,51 @@ n = Convert.ToInt32(Console.ReadLine());
 
 int[,] matrix = new int[m, n];
 
-for (int i = 0; i < m; i++)
-{
-    for (int j = 0; j < n; j++)
+int getRows(int m, int n) 
+{ 
+    for (int i = 0; i < m; i++)
     {
-        Console.WriteLine($"Введите значение для элемента {i}:{j}");
-        int value = Convert.ToInt32(Console.ReadLine());
-
-        matrix[i, j] = value;
-    }
-}
-
-int[] array = new int[m];
-
-
-
-for (int i = 0; i < m; i++)
-{
-    int plus = 0;
-    int minus = 0;
-    int zero = 0;
-
-    for (int j = 0; j < n; j++)
-    {
-        if (matrix[i, j] > 0)
+        for (int j = 0; j < n; j++)
         {
-            plus++;
-        }    
-        else if (matrix[i, j] == 0)
-        {
-            zero++;
-        }
-        else
-        {
-            minus++;
+            Console.WriteLine($"Введите значение для элемента {i}:{j}");
+            int value = Convert.ToInt32(Console.ReadLine());
+    
+            matrix[i, j] = value;
         }
     }
-
-    if (plus > minus)
+    
+    int[] array = new int[m];
+    
+    
+    
+    for (int i = 0; i < m; i++)
     {
-        int pos = i + 1;
-        Console.WriteLine($"Строка с большим количеством положительных числел: {pos}");
-    }
+        int plus = 0;
+        int minus = 0;
+        int zero = 0;
+    
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[i, j] > 0)
+            {
+                plus++;
+            }    
+            else if (matrix[i, j] == 0)
+            {
+                zero++;
+            }
+            else
+            {
+                minus++;
+            }
+        }
+    
+        if (plus > minus)
+        {
+            int pos = i + 1;
+            Console.WriteLine($"Строка с большим количеством положительных числел: {pos}");
+        }
+    return 1;
 }
+
+getRows(m, n);
